4da6dd3de84a09a1296308a287bf58f2f3f99e27
Niklas Leopold <leopold.niklas@gmail.com>
2023-04-08 09:24:04 +0200

    adding interactive slide

---
+++ examples/src/main/scala/kubernetes/session1/slides/Direction.scala
@@ -0,0 +1,9 @@
+package kubernetes.session1.slides
+
+sealed trait Direction
+case class DirectionUp() extends Direction
+case class DirectionDown() extends Direction
+case class DirectionRight() extends Direction
+case class DirectionLeft() extends Direction
+case class NoDirection() extends Direction
+
--- examples/src/main/scala/kubernetes/session1/slides/ExampleInteractiveSlide.scala
+++ examples/src/main/scala/kubernetes/session1/slides/ExampleInteractiveSlide.scala
@@ -1,79 +1,97 @@
 package kubernetes.session1.slides
 
-import cats.effect.{Ref, Temporal}
+import cats.effect.{Temporal}
 import cats.implicits._
 import com.github.morotsman.lote.algebra.{NConsole, Slide}
 import com.github.morotsman.lote.interpreter.nconsole.NConsole
 import com.github.morotsman.lote.interpreter.nconsole.NConsole.ScreenAdjusted
-import com.github.morotsman.lote.model.{Alignment, HorizontalAlignment, UserInput, VerticalAlignment}
+import com.github.morotsman.lote.model.{Alignment, Character, HorizontalAlignment, UserInput, VerticalAlignment}
+import cats.effect.std.Queue
 
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
-import scala.util.Random
+import scala.concurrent.duration.{DurationInt}
 
-object Direction extends Enumeration {
-  type VerticalAlignment = Value
-  val Up, Down, Left, Right, No = Value
-}
 
 object ExampleInteractiveSlide {
 
-  def make[F[_] : Temporal : NConsole](animator: Animator[F]): F[Slide[F]] = {
+  def make[F[_] : Temporal](animator: Animator[F]): F[Slide[F]] = {
 
-    for {
-      direction <- Ref[F].of(Direction.No)
-      slide = new Slide[F] {
-        override def content: NConsole[F] => F[NConsole.ScreenAdjusted] =
-          console => console.alignText("AWSD is your friend", Alignment(
-            VerticalAlignment.Center,
-            HorizontalAlignment.Center
-          ))
+    Temporal[F].pure(new Slide[F] {
+      override def content: NConsole[F] => F[NConsole.ScreenAdjusted] =
+        console => console.alignText("AWSD is your friend", Alignment(
+          VerticalAlignment.Center,
+          HorizontalAlignment.Center
+        ))
 
-        override def startShow: NConsole[F] => F[Unit] =
-          _ => animator.animate()
+      override def startShow: NConsole[F] => F[Unit] =
+        _ => animator.animate()
 
-        override def stopShow: F[Unit] =
+      override def stopShow: F[Unit] =
+        Temporal[F].unit
+
+      override def userInput(input: UserInput): F[Unit] = input match {
+        case Character(c) if c == 'a' =>
+          animator.changeDirection(DirectionLeft())
+        case Character(c) if c == 'w' =>
+          animator.changeDirection(DirectionUp())
+        case Character(c) if c == 's' =>
+          animator.changeDirection(DirectionDown())
+        case Character(c) if c == 'd' =>
+          animator.changeDirection(DirectionDown())
+        case _ =>
           Temporal[F].unit
-
-        override def userInput(input: UserInput): F[Unit] = {
-          animator.changeDirection(input)
-        }
       }
-    } yield slide
+    })
   }
 }
 
 trait Animator[F[_]] {
   def animate(): F[Unit]
-  def changeDirection(input: UserInput): F[Unit]
+  def changeDirection(input: Direction): F[Unit]
 }
 
 object Animator {
   def make[F[_] : Temporal : NConsole](): F[Animator[F]] = {
-    val animator = new Animator[F] {
+
+    def createAnimator(queue: Queue[F, Direction]): Animator[F] = new Animator[F] {
       override def animate(): F[Unit] = {
         for {
           screen <- NConsole[F].context
-          emptyScreen = List.fill(screen.screenWidth*screen.screenHeight)(' ')
-          _ <- loop(emptyScreen)
+          emptyScreen = Vector.fill(screen.screenWidth*screen.screenHeight)(' ')
+          _ <- loop(emptyScreen, Vector(
+            emptyScreen.length / 2,
+            emptyScreen.length / 2 + 1,
+            emptyScreen.length / 2 + 2,
+            emptyScreen.length / 2 + 3,
+            emptyScreen.length / 2 + 4,
+          ))
         } yield ()
       }
 
-      def loop(emptyScreen: List[Char]): F[Unit] = {
+      def loop(emptyScreen: Vector[Char], currentIndexes: Vector[Int]): F[Unit] = {
         for {
-          _ <- Temporal[F].sleep(40.milli)
+          maybeUserInput <- queue.tryTake
+          _ <- Temporal[F].sleep(100.milli)
           screen <- NConsole[F].context
+          updatedScreen = {
+            currentIndexes.foldRight(emptyScreen) { case (index, updatedScreen) =>
+              updatedScreen.updated(index, '*')
+            }
+          }
           _ <- NConsole[F].writeString(ScreenAdjusted(
-            // Random.nextInt(100).toChar.toString,
-            emptyScreen.mkString,
+            updatedScreen.mkString,
             screen.screenWidth,
             screen.screenHeight
           ))
-          _ <- loop(emptyScreen)
+          _ <- loop(emptyScreen, currentIndexes.map(_ - 1))
         } yield ()
       }
 
-      override def changeDirection(input: UserInput): F[Unit] = ???
+      override def changeDirection(input: Direction): F[Unit] =
+        queue.offer(input)
     }
-    Temporal[F].pure(animator)
+    for {
+      queue <- Queue.unbounded[F, Direction]
+      animator = createAnimator(queue)
+    } yield animator
   }
 }
