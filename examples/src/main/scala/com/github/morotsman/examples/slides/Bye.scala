9669129bb6c01ee7b1326b25a8c81ccbe73e008c
Niklas Leopold <leopold.niklas@gmail.com>
2023-04-16 10:05:18 +0200

    refactoring

--- examples/src/main/scala/com/github/morotsman/examples/slides/Bye.scala
+++ examples/src/main/scala/com/github/morotsman/examples/slides/Bye.scala
@@ -1,15 +1,5 @@
 package com.github.morotsman.examples.slides
 
-import cats.Monad
-import cats.effect.Temporal
-import cats.implicits._
-import com.github.morotsman.lote.algebra.{NConsole, Slide}
-import com.github.morotsman.lote.interpreter.nconsole.NConsole.ScreenAdjusted
-import com.github.morotsman.lote.model.{Alignment, HorizontalAlignment, UserInput, VerticalAlignment}
-
-import scala.concurrent.duration.DurationInt
-import scala.util.Random
-
 object Bye {
   def apply(): String =
     """
