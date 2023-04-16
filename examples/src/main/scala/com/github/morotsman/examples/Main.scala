90c682d8b3b7387549c58c2f8b28fe1c9b7dac4f
Niklas Leopold <leopold.niklas@gmail.com>
2023-04-16 10:00:00 +0200

    refactoring

--- examples/src/main/scala/com/github/morotsman/examples/Main.scala
+++ examples/src/main/scala/com/github/morotsman/examples/Main.scala
@@ -1,11 +1,10 @@
 package com.github.morotsman.examples
 
 import cats.effect._
-import com.github.morotsman.lote.algebra.NConsole
+import com.github.morotsman.lote.algebra.{Middleware, NConsole}
 import com.github.morotsman.lote.interpreter.PresentationExecutorInterpreter
 import com.github.morotsman.lote.interpreter.middleware.{Middleware, Timer}
 import com.github.morotsman.lote.interpreter.nconsole.NConsole
-import com.github.morotsman.lote.model.Presentation
 
 import scala.concurrent.duration.DurationInt
 
@@ -14,18 +13,27 @@ object Session1 extends IOApp.Simple {
 
   override def run(): IO[Unit] = {
 
-    def createPresentation(implicit console: NConsole[IO]): IO[Presentation[IO]] =
-      ExamplePresentation.make[IO]()
+    def createMiddleware(): IO[NConsole[IO]] = {
+      implicit val console: NConsole[IO] = NConsole.make[IO]()
+      for {
+        middleware <- Middleware.make[IO]()
+        timer <- Timer.make[IO](30.minutes)
+        _ <- middleware.addOverlays(List(timer))
+      } yield middleware
+    }
 
-    implicit val console: NConsole[IO] = NConsole.make[IO]()
+    def startPresentation(implicit console: NConsole[IO]): IO[Unit] =
+      for {
+        presentation <- ExamplePresentation.make[IO]()
+        executor <- PresentationExecutorInterpreter.make(presentation)
+        _ <- executor.start()
+      } yield ()
+
+
 
     for {
-      middleware <- Middleware.make[IO]()
-      timer <- Timer.make[IO](30.minutes)
-      _ <- middleware.addOverlays(List(timer))
-      presentation <- createPresentation(middleware)
-      executor <- PresentationExecutorInterpreter.make(presentation)
-      _ <- executor.start()
+      middleware <- createMiddleware()
+      _ <- startPresentation(middleware)
     } yield ()
   }
 
