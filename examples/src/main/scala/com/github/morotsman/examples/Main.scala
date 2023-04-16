ceb756d3449574db80fe332a812e55143c8a60c2
Niklas Leopold <leopold.niklas@gmail.com>
2023-04-16 09:33:15 +0200

    refactoring

--- examples/src/main/scala/com/github/morotsman/examples/Main.scala
+++ examples/src/main/scala/com/github/morotsman/examples/Main.scala
@@ -1,28 +1,29 @@
 package com.github.morotsman.examples
 
 import cats.effect._
+import com.github.morotsman.lote.algebra.NConsole
 import com.github.morotsman.lote.interpreter.PresentationExecutorInterpreter
 import com.github.morotsman.lote.interpreter.middleware.{Middleware, Timer}
 import com.github.morotsman.lote.interpreter.nconsole.NConsole
+import com.github.morotsman.lote.model.Presentation
 
 import scala.concurrent.duration.DurationInt
 
 
 object Session1 extends IOApp.Simple {
 
-  private val instruction1 =
-    """
-      |Supports different alignments
-      |""".stripMargin
-
   override def run(): IO[Unit] = {
-    implicit val console = NConsole.make[IO]()
+    
+    def createPresentation(implicit console: NConsole[IO]): IO[Presentation[IO]] =
+      ExamplePresentation.make[IO]()
+
+    implicit val console: NConsole[IO] = NConsole.make[IO]()
 
     for {
       middleware <- Middleware.make[IO]()
       timer <- Timer.make[IO](30.minutes)
       _ <- middleware.addOverlays(List(timer))
-      presentation <- ExamplePresentation.make[IO]()
+      presentation <- createPresentation(middleware)
       executor <- PresentationExecutorInterpreter.make(middleware, presentation)
       _ <- executor.start()
     } yield ()
