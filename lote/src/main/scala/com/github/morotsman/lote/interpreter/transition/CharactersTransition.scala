624a127126b46a178d9eb350b1f0db1767d00c20
Niklas Leopold <leopold.niklas@gmail.com>
2023-04-17 14:40:41 +0200

    refactoring

--- lote/src/main/scala/com/github/morotsman/lote/interpreter/transition/CharactersTransition.scala
+++ lote/src/main/scala/com/github/morotsman/lote/interpreter/transition/CharactersTransition.scala
@@ -15,12 +15,12 @@ case class Position(characters: List[CharacterPosition])
 
 object CharactersTransition {
 
-  def apply[F[_] : Temporal: NConsole](
-                              selectAccelerator: Double = 1.1,
-                              timeBetweenTicks: FiniteDuration = 40.milli,
-                              setupPosition: (Char, Char) => List[CharacterPosition],
-                              getNewIndex: (Screen, Int, CharacterPosition) => Option[Int]
-                            ): Transition[F] = new Transition[F] {
+  def apply[F[_] : Temporal : NConsole](
+                                         selectAccelerator: Double = 1.1,
+                                         timeBetweenTicks: FiniteDuration = 40.milli,
+                                         setupPosition: (Char, Char) => List[CharacterPosition],
+                                         getNewIndex: (Screen, Int, CharacterPosition) => Option[Int]
+                                       ): Transition[F] = new Transition[F] {
     override def transition(from: Slide[F], to: Slide[F]): F[Unit] = {
 
       def setupPositions(from: ScreenAdjusted, to: ScreenAdjusted): List[Position] =
@@ -34,10 +34,9 @@ object CharactersTransition {
         }.toList
 
       def transformPositions(
-                              screen: Screen,
                               currentCharacterPositions: List[Position],
                               positionsToUpdate: List[Int]
-                            ): List[Position] = {
+                            ): F[List[Position]] = {
         val toUpdate = currentCharacterPositions.toArray
 
         // mark positions to transform
@@ -55,37 +54,40 @@ object CharactersTransition {
           }
         }
 
-        // transform positions
-        currentCharacterPositions.zipWithIndex
-          .filter { case (position, _) =>
-            position.characters.exists(_.inTransition)
-          }
-          .foreach { case (position, index) =>
-            // take them away from the old position
-            toUpdate(index) = toUpdate(index).copy(characters = toUpdate(index).characters.filter(!_.inTransition))
-
-            // move to new position
-            val toMove = position.characters.filter(_.inTransition)
-            toMove.foreach { cp =>
-              val newIndex = getNewIndex(screen, index, cp);
-              newIndex.foreach { index =>
-                val updatedPosition = cp.copy(tick = cp.tick + 1)
-                if (
-                  index < toUpdate.length &&
-                    index >= 0 &&
-                    !toUpdate(index).characters.exists(_.character == '\n')) {
-                  toUpdate(index) = toUpdate(index).copy(characters = updatedPosition :: toUpdate(index).characters)
-                }
-              }
-
+        NConsole[F].context.map { screen =>
+          // transform positions
+          currentCharacterPositions.zipWithIndex
+            .filter { case (position, _) =>
+              position.characters.exists(_.inTransition)
             }
-          }
+            .foreach { case (position, index) =>
+              // take them away from the old position
+              toUpdate(index) = toUpdate(index).copy(characters = toUpdate(index).characters.filter(!_.inTransition))
 
-        toUpdate.toList
+              // move to new position
+              val toMove = position.characters.filter(_.inTransition)
+              toMove.foreach { cp =>
+                val newIndex = getNewIndex(screen, index, cp);
+                newIndex.foreach { index =>
+                  val updatedPosition = cp.copy(tick = cp.tick + 1)
+                  if (
+                    index < toUpdate.length &&
+                      index >= 0 &&
+                      !toUpdate(index).characters.exists(_.character == '\n')) {
+                    toUpdate(index) = toUpdate(index).copy(characters = updatedPosition :: toUpdate(index).characters)
+                  }
+                }
+
+              }
+            }
+
+          toUpdate.toList
+        }
+
+
       }
 
       def transformSlides(
-                           screen: Screen,
                            positions: List[Position],
                            randomPositions: List[Int],
                            nrUnderTransformation: Double = 1.0
@@ -95,26 +97,28 @@ object CharactersTransition {
         } else {
           val positionsToUpdate = randomPositions.take(nrUnderTransformation.toInt);
           val newRandomPositions = randomPositions.drop(nrUnderTransformation.toInt)
-          val updatedPositions = transformPositions(screen, positions, positionsToUpdate)
-          NConsole[F].clear() >>
-            NConsole[F].writeString(
+
+          for {
+            _ <- NConsole[F].clear()
+            updatedPositions <- transformPositions(positions, positionsToUpdate)
+            _ <- NConsole[F].writeString(
               ScreenAdjusted(
                 updatedPositions.map(_.characters.headOption.map(_.character).getOrElse("")).mkString("")
               )
-            ) >>
-            Temporal[F].sleep(timeBetweenTicks) >>
-            transformSlides(screen, updatedPositions, newRandomPositions, nrUnderTransformation * selectAccelerator)
+            )
+            _ <- Temporal[F].sleep(timeBetweenTicks)
+            _ <- transformSlides(updatedPositions, newRandomPositions, nrUnderTransformation * selectAccelerator)
+          } yield ()
         }
       }
 
       for {
         slide1 <- from.content
         slide2 <- to.content
-        screen <- NConsole[F].context
         positions = setupPositions(slide1, slide2)
         randomPositions = Random.shuffle(positions.indices.toList)
         _ <- NConsole[F].writeString(slide1) >>
-          transformSlides(Screen(screen.screenWidth, screen.screenHeight), positions, randomPositions) >>
+          transformSlides(positions, randomPositions) >>
           Temporal[F].sleep(200.milli) >>
           NConsole[F].writeString(slide2)
       } yield ()
